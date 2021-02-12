71a1f26303a4434e80143c558becf30a020d25e1
philipkarim <54434877+philipkarim@users.noreply.github.com>
2021-02-12 20:17:59 +0100

    uploading the project to github

---
+++ src/facemasks/data_facemasks.py
@@ -0,0 +1,131 @@
+import os
+import pickle
+import numpy as np
+from tensorflow.keras.preprocessing.image import img_to_array, load_img, ImageDataGenerator
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+
+class Data:
+    def __init__(self, X_train, X_test, y_train, y_test):
+        self.enc = LabelEncoder().fit(y_test)
+
+        if type(X_train) == np.ndarray:
+            self.X_train = X_train
+            self.y_train = y_train
+            self.y_train = to_categorical(self.enc.transform(self.y_train))
+
+        self.X_test = X_test
+        self.y_test_orig = y_test
+        
+        # One-hot encoding of y data
+        self.y_test = to_categorical(self.enc.transform(self.y_test_orig))
+        self.n_labels = len(self.enc.classes_)
+
+        self.gen = ImageDataGenerator(
+            rotation_range=10, zoom_range=0.10, width_shift_range=0.2, height_shift_range=0.2, horizontal_flip=True, fill_mode="nearest"
+        )
+        
+
+    def make_validation_set(self, val_size=0.25):
+        self.X_train, self.X_val, self.y_train, self.y_val = train_test_split(self.X_train, self.y_train, test_size=val_size)
+        self.n_training = self.X_train.shape[0]
+        self.n_validation = self.X_val.shape[0]
+
+
+    def postprocess(self, predictions):
+        prediction_objects = []
+        for array in predictions:
+            prediction_objects.append(Prediction(array, self.enc))
+
+        return prediction_objects
+    
+
+    def accuracy(self, predictions, print_report=False):
+        s = 0
+        for prediction, real in zip(predictions, self.y_test_orig):
+            if print_report:
+                print(f"Prediction:\t{prediction}\nConfidence:\t{prediction.confidence[prediction.val]}\nReal:\t\t{real}\n")
+            s += int(prediction == real)
+            
+        return s / len(predictions)
+
+
+    @staticmethod
+    def load_from_npy(dir):
+        X = np.concatenate((np.load(os.path.join(dir, "X_train.npy")), np.load(os.path.join(dir, "X_test.npy"))))
+        y = np.concatenate((np.load(os.path.join(dir, "y_train.npy")), np.load(os.path.join(dir, "y_test.npy"))))
+        
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        
+        return Data(X_train, X_test, y_train, y_test)
+
+
+    @classmethod
+    def load_from_imgs(cls, data_dir, return_img_paths=True):
+        img_paths = []
+        data = {}
+        X = []
+        y = []
+        for dirname, dirs, filenames in os.walk(os.path.join(data_dir)):
+            for filename in filenames:
+                if os.path.splitext(filename)[1] in [".png", ".jpg", ".jpeg"]:
+                    img_paths.append(os.path.join(dirname, filename))
+                
+        for img_path in img_paths:
+            label = img_path.split(os.path.sep)[-2]
+            img = preprocess_input(img_to_array(load_img(img_path, target_size=(224, 224))))
+            X.append(img)
+            y.append(label)
+            
+        if return_img_paths:
+            return cls(None, np.array(X), None, np.array(y)), img_paths
+            
+        return cls(None, np.array(X), None, np.array(y))
+                
+
+    @staticmethod
+    def save_from_imgs(data_dir):
+        for set_type in ["train", "test"]:
+            img_paths = []
+            X = []
+            y = []
+            for dirname, dirs, filenames in os.walk(os.path.join(data_dir, set_type)):
+                for filename in filenames:
+                    if os.path.splitext(filename)[1] in [".png", ".jpg", ".jpeg"]:
+                        img_paths.append(os.path.join(dirname, filename))
+                    
+            for img_path in img_paths:
+                label = img_path.split(os.path.sep)[-2]
+                img = preprocess_input(img_to_array(load_img(img_path, target_size=(224, 224))))
+                X.append(img)
+                y.append(label)
+                
+            np.save(os.path.join(data_dir, os.path.join("preprocessed", "X_" + set_type)), np.array(X))
+            np.save(os.path.join(data_dir, os.path.join("preprocessed", "y_" + set_type)), np.array(y))
+            
+            
+class Prediction:
+    def __init__(self, array, encoder):
+        max_val = 0
+        for i, val in enumerate(array):
+            if val > max_val:
+                max_val = val
+                max_i = i
+                
+        self.val = encoder.inverse_transform([max_i])[0]
+        
+        self.confidence = {}
+        for i, class_ in enumerate(encoder.classes_):
+            self.confidence[class_] = array[i]
+        
+    def __str__(self):
+        return self.val
+
+    def __eq__(self, other):
+        return self.val == other
+
+        
+if __name__ == "__main__":
+    data = Data.save_from_imgs("data")
---
+++ src/facemasks/predict.py
@@ -0,0 +1,43 @@
+# No warning or info messages from Tensorflow
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from skimage.transform import resize
+from skimage.io import imread
+from data_facemasks import Data
+
+import sys, inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from cnn import CNN
+
+OUT_DIR = os.path.join(os.path.dirname(parentdir), "doc/assets")
+
+cnn = CNN.load("models/facemask_cnn2")
+
+data = Data.load_from_npy("data/preprocessed")
+predictions = data.postprocess(cnn.predict(data.X_test))
+print(f"Test set accuracy: {data.accuracy(predictions)}")
+
+data, img_paths = Data.load_from_imgs("real_pics", return_img_paths=True)
+predictions = data.postprocess(cnn.predict(data.X_test))
+
+fig, axes = plt.subplots(1, 4, figsize=(6, 6))
+ax = axes.ravel()
+
+for ax_i, i in enumerate([1, 3, 5, 8]):
+    pic = imread(img_paths[i])
+    pic = np.asarray(pic)
+    pic = resize(pic, (224, 224), preserve_range=True, mode='constant')
+    pic /= 255
+    ax[ax_i].imshow(pic)
+    ax[ax_i].title.set_text(f"Solution = {data.y_test_orig[i]}\nPredicted = {predictions[i]}")
+
+fig.tight_layout()
+plt.show()
